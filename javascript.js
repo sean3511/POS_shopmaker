f33c1b30196126dfeac98638eb78010aaba9225c
Sloly <yahoo3511@gmail.com>
2023-01-12 05:00:30 +0800

    內容更新

--- javascript.js
+++ javascript.js
@@ -3,15 +3,17 @@
  var shop2_amount = 0;
  var shop3_amount = 0;
  var shop4_amount = 0;
- // 商品界面开启
- $('body:not(.cashier-shop-item-gp-right').on('click', '.cashier-shop-item', function(){
-    //開啟popup條件是沒有點到垃圾桶
-    // the clicked element doesn't have the foo class 
-  $('#overlay').fadeIn(100);
-  $('.shop-item-panel').fadeIn(100);
+
+ $(document).ready(function(){
+
+  if(shop_car_amount==0){
+    console.log("我有等於0");
+    $(".cashier-list").hide();
+    $(".payment-switch_block").show();
+  }
+  $(".cashier-btn").addClass("btn_glad_mode");
+  $(".cashier-btn p").html("NO SELL");
 });
-
-
   // 刪除商品
   $('.cashier-shop-itemsGP').on('click', '.remove-item', function(){
     $('#overlay').fadeOut(100);
@@ -35,17 +37,39 @@
     console.log("D商品4"+ shop4_amount);
   });
 
+ // 商品界面开启
+ $('body:not(.cashier-shop-item-gp-right').on('click', '.cashier-shop-item', function(){
+  //開啟popup條件是沒有點到垃圾桶
+  // the clicked element doesn't have the foo class 
+$('#overlay').fadeIn(100);
+$('.shop-item-panel').fadeIn(100);
+});
+//開啟購物車POPUP
+$('.menu-header-allitem').click(function(){
+  console.log(shop_car_amount);
+  $('#overlay').fadeIn(100);
+  $('.shop-car-GP').css("display", "flex").fadeIn(100);
+});
 
 
   // 关闭视窗
 $(document).mouseup(function(e)
   { 
-  var  pop = $('.shop-item-panel');  
+  var pop = $('.shop-item-panel,.shop-car-GP,.add-shop-car');  
   if(!pop.is(e.target) && pop.has(e.target).length === 0) { 
     $('#overlay').fadeOut(100);
-    $('.shop-item-panel').fadeOut(100);
+    $('.shop-item-panel,.shop-car-GP').fadeOut(100);
   }}
   );
+  // $(document).mouseup(function(e)
+  // { 
+  // var  pop = $('.shop-car-GP');  
+  // if(!pop.is(e.target) && pop.has(e.target).length === 0) { 
+  //   $('#overlay').fadeOut(100);
+  //   $('.shop-car-GP').fadeOut(100);
+  // }}
+  // );
+
 
 // if(documen.getElementsByClassName("frm1").length);
 
@@ -334,3 +358,57 @@ $('.menu-grid-item.shop1').click(function(){
           $('.customize-window').css('display', 'flex');
       }
     })
+    var shop_car_amount =0;
+    // document.querySelectorAll('.Shop-car-label').length+1;
+    // shop_car_amount =$('body').find($('.Shop-car-label')).length-1;
+    // shop_car_amount =$('body .Shop-car-label').length+1;
+    $(".add-shop-car").click(function(){
+
+      shop_car_amount++;
+
+      $(".cashier-btn").removeClass("btn_glad_mode");
+      $(".cashier-btn p").html("結帳");
+
+      if(shop_car_amount > 0){
+        console.log("我有等於1");
+        $(".cashier-list").show();
+        $(".payment-switch_block").hide();
+      }
+
+      console.log(shop_car_amount);
+      if(shop_car_amount==1){
+        $('.shop-car-list-GP').prepend(
+          )
+            .append(
+              $('<input>').prop({
+                type: 'radio',
+                id: 'shop-car'+shop_car_amount,
+                name: 'shop-car-list-panel',
+                  "checked":"checked"
+              }),
+              $('<label>').prop({
+                for: 'shop-car'+shop_car_amount
+              }).html('購物車'+shop_car_amount)
+              .addClass('Shop-car-label subtitle1')
+              .addClass('car'+shop_car_amount)
+            )
+      }
+      else
+      $('.shop-car-list-GP').prepend(
+        )
+          .append(
+            $('<input>').prop({
+              type: 'radio',
+              id: 'shop-car'+shop_car_amount,
+              name: 'shop-car-list-panel',
+            }),
+            $('<label>').prop({
+              for: 'shop-car'+shop_car_amount
+            }).html('購物車'+shop_car_amount)
+            .addClass('Shop-car-label subtitle1')
+            .addClass('car'+shop_car_amount)
+          )
+      
+    });
+
+  
